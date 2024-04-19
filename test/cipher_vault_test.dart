95b84da79f6c9e5ecc25b482e45c974db5ea1e94
MixKage <shishov265@gmail.com>
2024-04-19 16:07:44 +0300

    add: tests

--- test/cipher_vault_test.dart
+++ test/cipher_vault_test.dart
@@ -1,12 +1,47 @@
-// import 'package:flutter_test/flutter_test.dart';
-
-// import 'package:cipher_vault/cipher_vault.dart';
+import 'package:cipher_vault/cipher_vault.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  // test('adds one to input values', () {
-  //   final calculator = Calculator();
-  //   expect(calculator.addOne(2), 3);
-  //   expect(calculator.addOne(-7), -6);
-  //   expect(calculator.addOne(0), 1);
-  // });
+  group('CipherVault widget tests', () {
+    testWidgets('Widget displays random animation', (final tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: CipherVault(),
+        ),
+      );
+
+      await tester.pump();
+
+      expect(find.text(''), findsNothing);
+    });
+
+    testWidgets('Widget displays secret', (final tester) async {
+      const secret = 'My Secret';
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: CipherVault(secret: secret),
+        ),
+      );
+
+      await tester.pump(const Duration(seconds: 4));
+
+      expect(
+        find.text(secret),
+        findsOneWidget,
+      );
+    });
+
+    testWidgets('Widget timer test', (final tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: CipherVault(),
+        ),
+      );
+
+      await tester.pump(const Duration(seconds: 1));
+
+      expect(find.byType(CipherVault), findsOneWidget);
+    });
+  });
 }
