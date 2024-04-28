7a1325ad33af87a332aa191e59a774163644eed7
MixKage <shishov265@gmail.com>
2024-04-28 14:24:26 +0300

    add: example | finish work

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:cipher_vault/cipher_vault.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +9,142 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'CipherVault Example',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // You can use a different alphabet.
+  static const String cyrillicAlphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+      'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ!@#%^&*()-_=+';
+  late bool isShowSecret;
+  late String? secret1;
+  late String? secret2;
+  late String? secret3;
+  // You can create, store, and modify Config using copyWith and CopyWithClass
+  late CipherVaultConfig config;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    secret1 = null;
+    secret2 = null;
+    secret3 = null;
+    isShowSecret = true;
+    config = const CipherVaultConfig(
+      minLengthCipherVault: 10,
+      maxLengthCipherVault: 22,
+      showPasswordAnimationFrequency: 0.8,
+    );
+    super.initState();
+  }
+
+  Future<void> _getSecret() async {
+    // Getting the secret with Future
+    await Future.delayed(
+      const Duration(seconds: 1),
+      () => setState(
+        () {
+          secret1 = isShowSecret ? 'My Secret 1' : null;
+          secret2 = isShowSecret ? 'My Long Secret 2' : null;
+          secret3 = isShowSecret ? 'Мой длинный секрет 3' : null;
+          isShowSecret = !isShowSecret;
+        },
+      ),
+    );
+  }
+
+  Future<void> _copyButtonSecret() async {
+    Clipboard.setData(ClipboardData(text: secret1 ?? '')).then(
+      (_) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('The text has been copied'),
+          ),
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('CipherVault Example'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CipherVault(
+                secret: secret1,
+              ),
+              CipherVault(
+                secret: secret2,
+                textStyle: const TextStyle(fontSize: 16.0),
+                config: config,
+                buttonCopy: IconButton(
+                  padding: EdgeInsets.zero,
+                  iconSize: 16,
+                  onPressed: _copyButtonSecret,
+                  icon: const Icon(Icons.copy),
+                ),
+              ),
+              // Example with another alphabet
+              CipherVault(
+                secret: secret3,
+                textStyle: const TextStyle(fontSize: 18.0),
+                config: config.copyWith(
+                  alphabetCipherVault: cyrillicAlphabet,
+                  countAttemp: 15,
+                  showAnimationFrequency: 3.0,
+                  updateAnimationUpdate: const Duration(milliseconds: 30),
+                ),
+                buttonCopy: IconButton(
+                  padding: EdgeInsets.zero,
+                  iconSize: 18,
+                  onPressed: _copyButtonSecret,
+                  icon: const Icon(Icons.copy),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: _getSecret,
+                child: const Text('Get Secret'),
+              ),
+            ],
+          ).withPadding(const EdgeInsets.all(16.0)),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+extension ColumnPadding on Column {
+  Column withPadding(EdgeInsetsGeometry padding) {
+    return Column(
+      children: children
+          .map((widget) => Padding(
+                padding: padding,
+                child: widget,
+              ))
+          .toList(),
     );
   }
 }
